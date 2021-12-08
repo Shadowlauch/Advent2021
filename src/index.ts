ff0bbae16485862740ba9e33b75213cbdf983fa6
Lars Naurath <lars.naurath@gmail.com>
2021-12-08 17:42:57 +0100

    improved start script

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import {readdir} from 'fs/promises';
+import {readdir, writeFile, mkdir, copyFile} from 'fs/promises';
 import {join} from 'path';
+import {Command} from 'commander';
+import {execSync} from 'child_process';
 
 (async () => {
   const dirs = await readdir(__dirname, {withFileTypes: true}).then(items => {
@@ -7,9 +9,34 @@ import {join} from 'path';
       .sort((a, b) =>
         parseInt(b.name.replace('day', '')) -  parseInt(a.name.replace('day', '')))
   });
-  const dir = process.argv[2] ?? dirs[0].name;
 
-  import(join(__dirname, dir, 'index.ts'));
+  const program = new Command();
+
+  program
+    .command('run [dir]')
+    .option('-e, --example', 'run on example data')
+    .action(async (dirArg, opts) => {
+      const dir = dirArg ?? dirs[0].name;
+      const isExample = opts.example ?? false;
+
+      const module = await import(join(__dirname, dir, 'index.ts'));
+
+      if (module.hasOwnProperty('default')) module.default(isExample ? 'example.txt' : 'input.txt');
+    });
+
+  program
+    .command('create')
+    .action(async () => {
+      const newDir = `day${parseInt(dirs[0].name.replace('day', '')) + 1}`;
+      await mkdir(join(__dirname, newDir));
+      await copyFile(join(__dirname, 'utils', 'template.txt'), join(__dirname, newDir, 'index.ts'));
+      await writeFile(join(__dirname, newDir, 'example.txt'), '');
+      await writeFile(join(__dirname, newDir, 'input.txt'), '');
+
+      execSync('git add .', {cwd: join(__dirname, newDir)});
+    });
+
+  program.parse();
 })();
 
 
